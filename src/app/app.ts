e1610b9018e42e07cd1e2aa1db33f1aa8022bd8c
M373746 <j.beskow@sistran.com.br>
2026-07-02 15:00:18 -0300

    creating angular project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { Pagina } from './pagina/pagina';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Pagina],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })

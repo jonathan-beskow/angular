170a613f5e96302bfa2d7409386453e2668ed9c8
M373746 <j.beskow@sistran.com.br>
2026-07-03 11:22:11 -0300

    adding style

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { Pagina } from './pagina/pagina';
+import { Calculadora } from './calculadora/calculadora';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Pagina],
+  imports: [Calculadora],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })

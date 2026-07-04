8b0f866a3c12f523f40a60eba03e27f05b5a103f
M373746 <j.beskow@sistran.com.br>
2026-07-04 09:55:49 -0300

    creating new component

---
+++ src/app/lista-compras/lista-compras.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-compras',
+  imports: [],
+  templateUrl: './lista-compras.html',
+  styleUrl: './lista-compras.scss',
+})
+export class ListaCompras {}

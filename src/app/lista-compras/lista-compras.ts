b2b7bf2ca5e138cb2ea15416d75954d4282dc7d0
M373746 <j.beskow@sistran.com.br>
2026-07-05 13:15:11 -0300

    including for function

--- src/app/lista-compras/lista-compras.ts
+++ src/app/lista-compras/lista-compras.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ItemLista } from './itemlista';
 
 @Component({
   selector: 'app-lista-compras',
-  imports: [],
+  imports: [ FormsModule],
   templateUrl: './lista-compras.html',
   styleUrl: './lista-compras.scss',
 })
-export class ListaCompras {}
+export class ListaCompras {
+
+  item: string = '';
+  lista: ItemLista[] = [];
+
+  adicionarItem(){
+    let itemLista = new ItemLista();
+    itemLista.nome = this.item;
+    itemLista.id = this.lista.length + 1;
+    this.lista.push(itemLista);
+
+    this.item = '';
+  }
+
+}
